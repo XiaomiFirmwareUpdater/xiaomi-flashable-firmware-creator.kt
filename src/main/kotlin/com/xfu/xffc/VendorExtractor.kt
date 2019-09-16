748f54a932d6516378e585deafafeb9c2c5821d6
yshalsager <ysh-alsager@hotmail.com>
2019-09-16 12:50:06 +0200

    Add mtk support to VendorExtractor
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- src/main/kotlin/com/xfu/xffc/VendorExtractor.kt
+++ src/main/kotlin/com/xfu/xffc/VendorExtractor.kt
@@ -14,18 +14,27 @@ class VendorExtractor(process: Process) : BaseExtractor(process) {
 
     private fun vendorFilter(): List<String> {
         return zipContent.filterNot {
-            it.startsWith("system") || it.startsWith("boot.img")
+            it.startsWith("system") || it.startsWith("boot.img") || it.endsWith('/')
         }
     }
 
     private fun vendorUpdaterScript(): MutableList<String> {
         val updaterScriptLines: MutableList<String> = mutableListOf()
         File("tmp/META-INF/com/google/android/updater-script").useLines { lines ->
-            lines.forEach {
-                if (it.contains("getprop") || it.contains("Target") ||
-                    it.contains("firmware-update") || it.contains("vendor")
-                ) {
-                    updaterScriptLines.add(it)
+            if (romType == "qcom") {
+                lines.forEach {
+                    if (it.contains("getprop") || it.contains("Target") ||
+                        it.contains("firmware-update") || it.contains("vendor")
+                    ) {
+                        updaterScriptLines.add(it)
+                    }
+                }
+            } else {
+                lines.forEach {
+                    if (!it.contains("system") || !it.contains("boot.img")
+                    ) {
+                        updaterScriptLines.add(it)
+                    }
                 }
             }
         }
